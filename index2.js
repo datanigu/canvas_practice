75ca3dd7252043f0ff767b44d6ac7b7cdad37821
Darcy Taniguchi <datanigu@users.noreply.github.com>
2017-08-31 21:04:18 -0700

    Things with buttons, like mouseenter makes button change color

--- index2.js
+++ index2.js
@@ -23,11 +23,6 @@ window.onload = () => {
   image_add.src = "plankton_pics/SPCP2-1501025702-061553-000-272-2468-208-235.png";
 
 // Making imaeges move
-let element
-let x
-let y
-let sx
-let sy
 const move_mutliplier = 0.5 // multiple to determine how much random walk will move
 const step = (timestamp, elements, duration) =>{
   const delta_time = timestamp - init_time
@@ -57,15 +52,11 @@ const step = (timestamp, elements, duration) =>{
 let init_time
 const element_array = [
   {element: image, x:10,y:10, dx:150, dy:150},
-  {element: image2, x:10, y:300, dx:150, dy:150},
-  {element: image_add, x:10, y:600, dx:150, dy:150}
+  {element: image2, x:10, y:200, dx:150, dy:150},
+  {element: image_add, x:10, y:350, dx:150, dy:150}
 ]
-const duration_time = 6000
-window.requestAnimationFrame(time =>{
-  init_time = time
-  step(time, element_array, duration_time)
-})
 
+const duration_time = 6000
 
 // Putting in table of images
     const my_pics = [
@@ -105,33 +96,79 @@ let myTable
   }
     myTable += "</tr></table>";
 
-// Uncomment line below if want table of images to show up
-    document.getElementById('tablePrint').innerHTML = myTable
+    const body = document.getElementsByTagName("body")[0];
 
-// Button to go to a different page
-    document.getElementById("button1").onclick = () =>{
-      location.href = "http://www.darcytaniguchi.com"
-    }
+    // Uncomment line below if want table of images to show up
+        // const table_of_pics = document.createElement('table')
+        // table_of_pics.style.position = "absolute"
+        // table_of_pics.style.top = "190px"
+        // table_of_pics.style.left = "200px"
+        // table_of_pics.innerHTML = myTable
+        // body.appendChild(table_of_pics)
 
-    function visitPage(){
-        window.location='http://www.google.com';
-    }
-
+// Inserting button
     // 1. Create the button
-    var button4 = document.createElement("button")
+    const button4 = document.createElement("button")
     button4.style.backgroundColor = "white"
     button4.style.position="absolute"
     button4.style.top = "40px"
     button4.style.left = "150px"
-
     button4.innerHTML = "Equation of unit circle"
 
     // 2. Append somewhere
-    var body = document.getElementsByTagName("body")[0];
     body.appendChild(button4)
 
     // 3. Add event handler
-    button4.addEventListener ("click", function() {
+    button4.addEventListener ("click", ()=> {
       alert("x^2 + y^2 = 1")
     })
-  }
+
+    // Trying to use image as button
+    const image_button = new Image()
+    image_button.src = "plankton_pics/SPCP2-1501025702-061553-000-272-2468-208-235.png"
+    image_button.style.position="absolute"
+    image_button.style.top = "10px"
+    image_button.style.left = '400px'
+    image_button.style.width = 100
+    image_button.style.height=100
+    // ctx.drawImage(image_button, 30,20, 500,500)
+    image_button.onclick = () =>{
+      window.location.href = 'http://www.darcytaniguchi.com'
+    }
+    document.body.appendChild(image_button)
+
+    // const button_position = (position_type, element, top, left, width, height){
+    //   element.style.position = position_type
+    // }
+
+
+// Button that does multiple things
+    const add_img_and_date  = () =>{
+      ctx.drawImage(image,200,200,90,90)
+      ctx.fillText(Date(),400,300)
+    }
+
+    const button5 = document.createElement("button")
+    const change_color = (element, color) =>{
+      return element.style.backgroundColor = color
+    }
+
+    button5.innerHTML = "Button experimentation"
+    body.appendChild(button5)
+    button5.addEventListener('click', add_img_and_date, false)
+    button5.addEventListener('mouseenter',  event =>{
+      event.target.style.backgroundColor = "blue"
+      setTimeout( ()=> {
+        event.target.style.backgroundColor=""},1000)
+    }, false)
+    // button5.addEventListener('mouseover', change_color(button5, 'magenta'), false)
+  }// end of window.onload
+
+  /*
+Other ideas to consider:
+ - Responsive resizing
+  - Consider looking here https://codepen.io/cRckls/pen/mcGCL?editors=0010#0
+  - Maybe also look here https://stackoverflow.com/questions/1297449/change-image-size-with-javascript
+- Buttons change color when hovered over and when depressed
+- Having tabs that lead to different pages
+  */
